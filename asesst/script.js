d7f134e495df7f888a5d691a92b313e07fc977d4
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-04 11:54:54 +0100

    Modified a checked button

--- asesst/script.js
+++ asesst/script.js
@@ -3,14 +3,40 @@ let todo = document.querySelector(".todo-list");
 
 let todoList = [];
 
-if(localStorage.getItem("todo")) {
+if (localStorage.getItem("todo")) {
     todoList = JSON.parse(localStorage.getItem("todo"));
     displayMessages();
 }
 
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('check-btn')) {
+        button = todoList.find(item => "item_" + item.id === e.target.id);
+        if (button.checked) {
+            button.checked = false
+            console.log(button.checked)
+            e.target.classList.remove("done-btn");
+            e.target.children[0].classList.add("noTick");
+
+        } else {
+            button.checked = true
+            console.log(button.checked)
+            e.target.classList.add("done-btn");
+            e.target.children[0].classList.remove("noTick");
+        }
+
+
+
+
+        // console.log(e.target.id);
+        // console.log("item_" + todoList[0].id);
+
+    }
+})
+
 addMessage.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         let newTodo = {
+            id: new Date().getTime(),
             todo: addMessage.value,
             checked: false,
             visibility: true
@@ -30,8 +56,11 @@ function displayMessages() {
     todoList.forEach(function (item, i) {
         displayMessage += `
         <div class="todo-item">
-                <div class="flex-item" id='item_${i}'>
-                    <button class="check-btn">
+                <div class="flex-item" >
+                    <button class="check-btn" id='item_${item.id}'>
+                        <svg class="noTick" xmlns="http://www.w3.org/2000/svg" width="10" height="8">
+                        <path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6" />
+                        </svg>
                     </button>
                     <p>${item.todo}</p>
                 </div>
@@ -50,6 +79,15 @@ function displayMessages() {
 
 
 
+
+
+// function toChangeButton(data) {
+//     for (let index in data) {
+//         console.log(data[index].done);
+//         console.log(data[index].id);
+//         isDone(data[index].done, data[index].id);
+//     }
+// };
 
 
 
@@ -79,29 +117,6 @@ function displayMessages() {
 
 
 
-
-// data = [
-//     {
-//         id: 1,
-//         text: "Jog arround the park",
-//         done: false,
-//         visibility: true
-//     },
-//     {
-//         id: 2,
-//         text: "Read for 1 hour",
-//         done: true,
-//         visibility: true
-//     },
-//     {
-//         id: 3,
-//         text: "Meditation",
-//         done: true,
-//         visibility: true
-//     },
-// ]
-
-
 // inputText.addEventListener("keypress", function (event) {
 //     if (event.key === "Enter") {
 //         let newText = inputText.value;
@@ -119,42 +134,6 @@ function displayMessages() {
 //         inputText.value = "";
 //     }
 // });
-
-
-// // the function displays all items fron todolist
-// function displayTodoList(data) {
-//     if (!data || data.length === 0) {
-//         //   productContainer.innerHTML = "<h2 class='no-results'>No Results</h2>";
-//     } else {
-//         const todoList = data
-//             .map((todoItem) => {
-//                 const { id, text, done, visibility } = todoItem;
-//                 console.log(todoItem);
-//                 // isDone(done, id);
-//                 return `
-//           <div class="todo-item">
-//           <div class="flex-item">
-//               <button class="check-btn">
-//               </button>
-//               <p class="text-task">${text}</p>
-//           </div>
-
-//           <button class="cross-btn">
-//               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-//                   <path fill="#494C6B" fill-rule="evenodd"
-//                       d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" />
-//               </svg>
-//           </button>
-//       </div>
-//     `;
-//             })
-//             .join("");
-//         // console.log(data[1].text)
-//         // toChangeButton(data);
-//         displayTodoListEl.innerHTML = todoList;
-//     }
-// }
-
 
 
 // function isDone(done, id) {
